e42fcf11bc5ba69d5729d232cdd2612ff19940e9
IrvinMitre <mitre_ismm@outlook.com>
2017-11-08 00:12:35 -0600

    error, pero avences con las migraciones y busquedas

--- resources/views/Proyectos/proyect.blade.php
+++ resources/views/Proyectos/proyect.blade.php
@@ -4,8 +4,9 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">  {{ Auth::user()->name }}, tu particion aqui sigue</div>
+            <div class="panel panel-default"> {{ Auth::user()->name }}, tu particion aqui sigue
+                <div class="panel-heading"> </div>
+                
                 <div class="panel-body">
 
                 </div>
